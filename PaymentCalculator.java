7ba6a3ca8acccc3d4c54cb71460a182d678417bf
Kodeddy <edidiongdan1@gmail.com>
2023-12-11 13:18:32 +0100

    Created PaymentCalculator.java file

---
+++ PaymentCalculator.java
@@ -0,0 +1,13 @@
+package com.example.firstjavaprogram;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class PaymentCalculator {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
